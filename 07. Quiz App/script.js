82022ae389cb77ae869700231a29a882aa23a606
Manisangh Sharma <78167300+manisanghsharma@users.noreply.github.com>
2023-10-07 14:45:22 +0530

    fixed the code

--- 07. Quiz App/script.js
+++ 07. Quiz App/script.js
@@ -1,45 +1,46 @@
+const question = document.getElementById("question");
+const answerButtons = document.getElementById("answer");
+const nextButton= document.getElementById("next-btn");
+
+
 const questions = [
     {
-        question: "Which is larget animal in the world?",
+        question: "Which is the larget animal in the world?",
         answers: [
-        { text: "Shark", correct: false},
-        { text: "Blue Whale", correct: true},
-        { text: "Elephant", correct: false},
-        { text: "Giraffe", correct: false},
+            { text: "Shark", correct: false},
+            { text: "Blue Whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Giraffe", correct: false},
         ]
     },
     {
         question: "Which is the smallest country in the world?",
         answers: [
-        { text: "Vatican City", correct: false},
-        { text: "Bhutan", correct: true},
-        { text: "Nepal", correct: false},
-        { text: "Giraffe", correct: false},
+            { text: "Vatican City", correct: true},
+            { text: "Bhutan", correct: false},
+            { text: "Nepal", correct: false},
+            { text: "Maldives", correct: false},
         ]
     },
     {
         question: "Which is the largest desert in the world?",
         answers: [
-        { text: "Kalahari", correct: false},
-        { text: "Gobi", correct: true},
-        { text: "Sahara", correct: false},
-        { text: "Antarctica", correct: true},
+            { text: "Kalahari", correct: false},
+            { text: "Gobi", correct: false},
+            { text: "Sahara", correct: true},
+            { text: "Antarctica", correct: false},
         ]
     },
     {
         question: "What is the smallest continent in the world?",
         answers: [
-        { text: "Asia", correct: false},
-        { text: "Australia", correct: true},
-        { text: "Arctic", correct: false},
-        { text: "Africa", correct: false},
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arctic", correct: false},
+            { text: "Africa", correct: false}, 
         ]
     }
 ]
-
-const questionElement = document.getElementById("questions");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton= document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -52,16 +53,24 @@ function startQuiz(){
 }
 
 function showQuestion(){
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + "." +currentQuestion.question;
+    question.innerHTML = questionNo + "." + currentQuestion.question;
 
-    currentQuestion.answers.forEach(answer => {
+    currentQuestion.answers.forEach(a => {
     const button = document.createElement("button");
-    button.innerHTML = answer.text;
+    button.innerHTML = a.text;
     button.classList.add("btn");
     answerButtons.appendChild(button);
-    })
+
+    if(a.correct){
+        button.dataset.correct = a.correct;
+    }
+
+    button.addEventListener("click", selectAnswer);
+    });
+
 }
 
 function resetState(){
@@ -69,6 +78,52 @@ function resetState(){
     while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild);
     }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+
+
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+})
+
+function showScore(){
+    resetState();
+    question.innerHTML = `You scored ${score} out of ${questions.length}!`
+    nextButton.innerHTML = "Play again";
+    nextButton.style.display = "block";
 }
 
 startQuiz();
