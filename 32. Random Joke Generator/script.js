8bfd9ace97a50e54d96129b114dbe0c72b75382c
Manisangh Sharma <78167300+manisanghsharma@users.noreply.github.com>
2023-10-19 08:50:47 +0530

    made the application responsive

--- 32. Random Joke Generator/script.js
+++ 32. Random Joke Generator/script.js
@@ -2,22 +2,22 @@ const setup = document.getElementById("setup");
 const punchline = document.getElementById("punchline");
 const generateBtn = document.getElementById("generate");
 
-const apiUrl = "https://official-joke-api.appspot.com/jokes/programming/random";
+const apiUrl = "https://official-joke-api.appspot.com/random_joke";
 
-async function getJoke(){
-    try{
-        let response = await fetch(apiUrl);
-        let data = await response.json();
-        setup.innerHTML = "Q. " + data[0].setup;
-        punchline.innerHTML = data[0].punchline;
-    }
-    catch(error){
-        alert("Something is wrong with the server :(");
-    }
+async function getJoke() {
+  try {
+    let response = await fetch(apiUrl);
+    let data = await response.json();
+    setup.innerHTML =
+      data.setup.indexOf("?") === -1 ? data.setup : "Q. " + data.setup;
+    punchline.innerHTML = data.punchline;
+  } catch (error) {
+    alert("Something is wrong with the server :(");
+  }
 }
 
 getJoke();
 
 generateBtn.addEventListener("click", () => {
-    getJoke();
-})
+  getJoke();
+});
